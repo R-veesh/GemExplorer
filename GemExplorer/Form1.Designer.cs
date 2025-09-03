00cc249814b0e812a761d043364de368c578399c
R-veesh <raweeshalakruvan5@gmail.com>
2025-09-04 03:02:03 +0530

    gride, buttons, logic, accest impliment

--- GemExplorer/Form1.Designer.cs
+++ GemExplorer/Form1.Designer.cs
@@ -29,13 +29,56 @@ namespace GemExplorer
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(36, 65);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(322, 474);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Score: 0";
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Location = new System.Drawing.Point(685, 12);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(75, 23);
+            this.BtnStart.TabIndex = 1;
+            this.BtnStart.Text = "Start";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- GemExplorer/Form1.cs
+++ GemExplorer/Form1.cs
@@ -1,20 +1,116 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GemExplorer
 {
     public partial class Form1 : Form
     {
+        private const int gridSize = 10;
+        private const int cellSize = 50;
+        private Button[,] gridButtons = new Button[gridSize, gridSize];
+        private Random rand = new Random();
+        private int playerX = 0, playerY = 0;
+        private int score = 0;
+
         public Form1()
         {
             InitializeComponent();
+            InitUI();
+        }
+
+        private void InitUI()
+        {
+            // Panel settings
+            panel1.Size = new Size(gridSize * cellSize, gridSize * cellSize);
+            panel1.Location = new Point(20, 60);
+
+            // Label settings
+            label1.Text = "Score: 0";
+            label1.Font = new Font("Arial", 14, FontStyle.Bold);
+            label1.Location = new Point(20, 20);
+
+            // Button settings
+            BtnStart.Text = "Start Game";
+            BtnStart.Location = new Point(200, 20);
+            BtnStart.Click += BtnStart_Click;
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+            score = 0;
+            playerX = 0; playerY = 0;
+            InitGrid();
+            UpdatePlayerPosition();
+            label1.Text = "Score: 0";
+        }
+
+        private void InitGrid()
+        {
+            for (int x = 0; x < gridSize; x++)
+            {
+                for (int y = 0; y < gridSize; y++)
+                {
+                    Button btn = new Button();
+                    btn.Size = new Size(cellSize, cellSize);
+                    btn.Location = new Point(x * cellSize, y * cellSize);
+                    btn.Tag = "empty";
+                    btn.BackgroundImageLayout = ImageLayout.Stretch;
+
+                    // Randomly place gems
+                    if (rand.Next(0, 100) < 15)
+                    {
+                        btn.Tag = "diamond";
+                        btn.BackgroundImage = Properties.Resources.gem; // gem.png in Resources
+                    }
+
+                    gridButtons[x, y] = btn;
+                    panel1.Controls.Add(btn);
+                }
+            }
+        }
+
+        private void UpdatePlayerPosition()
+        {
+            // Reset images
+            for (int x = 0; x < gridSize; x++)
+            {
+                for (int y = 0; y < gridSize; y++)
+                {
+                    if (gridButtons[x, y].Tag.ToString() == "diamond")
+                        gridButtons[x, y].BackgroundImage = Properties.Resources.gem;
+                    else
+                        gridButtons[x, y].BackgroundImage = null;
+                }
+            }
+
+            // Collect gem
+            if (gridButtons[playerX, playerY].Tag.ToString() == "diamond")
+            {
+                score++;
+                gridButtons[playerX, playerY].Tag = "empty";
+                label1.Text = $"Score: {score}";
+            }
+
+            // Show player
+            gridButtons[playerX, playerY].BackgroundImage = Properties.Resources.player; // player.png in Resources
+        }
+
+  
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            switch (keyData)
+            {
+                case Keys.Up: if (playerY > 0) playerY--; break;
+                case Keys.Down: if (playerY < gridSize - 1) playerY++; break;
+                case Keys.Left: if (playerX > 0) playerX--; break;
+                case Keys.Right: if (playerX < gridSize - 1) playerX++; break;
+            }
+
+            UpdatePlayerPosition();
+            return true;
         }
     }
 }
